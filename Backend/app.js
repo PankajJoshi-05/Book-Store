f07ec7048a9a947489ecdf1b3c0c67be97688290
unknown <joshipankaj9456@gmail.com>
2024-11-11 22:13:48 +0530

    Added Cart and Favourites functionality

--- Backend/app.js
+++ Backend/app.js
@@ -4,10 +4,14 @@ require("dotenv").config();
 require("./Connection/Connection")
 const user =require("./routes/user");
 const book=require("./routes/book");
+const fav=require("./routes/fav");
+const cart=require("./routes/cart");
 app.use(express.json());
 
 app.use("/api/v1",user);
 app.use("/api/v1",book);
+app.use("/api/v1/",fav);
+app.use("/api/v1",cart);
 
 app.listen(process.env.PORT,()=>{
     console.log(`server started ${ process.env.PORT}`);
